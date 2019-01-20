988a33e2d87697f9d16af73ba60b0ac3508a16d8
Ethan.P <ethanpui@gmail.com>
2019-01-20 21:58:37 +0800

    1st update

--- app/src/main/java/com/example/soundboard/RandomActivity.java
+++ app/src/main/java/com/example/soundboard/RandomActivity.java
@@ -1,31 +1,30 @@
 package com.example.soundboard;
 
-import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class RandomActivity extends AppCompatActivity {
 
-    Button RandomBtn;
+    ImageButton OfBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        RandomBtn = (Button) findViewById(R.id.RandomBtn); //id of COD Button
+        setContentView(R.layout.activity_random);
+        OfBtn = (ImageButton) findViewById(R.id.OfBtn);
 
-        RandomBtn.setOnClickListener(new View.OnClickListener() {
+        OfBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,RandomActivity.class));
-
-
-
+                MediaPlayer mp = MediaPlayer.create(RandomActivity.this,R.raw.ooof);
+                        mp.start();
             }
         });
+
 
     }
 }
