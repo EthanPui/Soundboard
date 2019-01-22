6b501a80e3570cb04049139b42a56c99654df432
Ethan.P <ethanpui@gmail.com>
2019-01-22 12:18:18 +0800

    7th Update
    Sucessfull Implementation of all Buttons, each directing userto new seperate activites

--- app/src/main/java/com/example/soundboard/MainActivity.java
+++ app/src/main/java/com/example/soundboard/MainActivity.java
@@ -9,25 +9,68 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button GamesBtn, RandomBtn;
+    Button GamesBtn, MoviesBtn, PoliticsBtn, ViralBtn, MemesBtn, TelevisionBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         GamesBtn = (Button) findViewById(R.id.GamesBtn); //id of Button
+        MoviesBtn = (Button) findViewById(R.id.MoviesBtn);
+        PoliticsBtn = (Button) findViewById(R.id.PoliticsBtn);
+        ViralBtn = (Button) findViewById(R.id.ViralBtn);
+        MemesBtn = (Button) findViewById(R.id.MemesBtn);
+        TelevisionBtn = (Button) findViewById(R.id.TelevisionBtn);
 
         GamesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, GamesActivity.class));
 
+                MoviesBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity.this, MoviesActivity.class));
+
+                        PoliticsBtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startActivity(new Intent(MainActivity.this, PoliticsActivity.class));
+
+                                ViralBtn.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        startActivity(new Intent(MainActivity.this, ViralActivity.class));
+
+                                        MemesBtn.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                startActivity(new Intent(MainActivity.this, MemesActivity.class));
+
+                                                TelevisionBtn.setOnClickListener(new View.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(View v) {
+                                                        startActivity(new Intent(MainActivity.this, TelevisionActivity.class));
+
+
+                                                    }
+                                                });
+
+
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
             }
         });
+    }
+}
 
 
-            }
-        }
 
 
 
