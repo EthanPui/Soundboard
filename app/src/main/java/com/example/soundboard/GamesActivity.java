37fad3e7e34392c4729f684a2f5832ecf220d8f7
Ethan.P <ethanpui@gmail.com>
2019-02-03 13:33:53 +0800

    10th Update
    Started on GamesActivity
    1. added missionfailed and ooof buttons with mp3 set for both.

--- app/src/main/java/com/example/soundboard/GamesActivity.java
+++ app/src/main/java/com/example/soundboard/GamesActivity.java
@@ -4,24 +4,37 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+
 
 public class GamesActivity extends AppCompatActivity {
 
-    ImageButton OfBtn;
+    Button  MissionFailedBtn, OoofBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
-        OfBtn = (ImageButton) findViewById(R.id.OfBtn);
 
-        OfBtn.setOnClickListener(new View.OnClickListener() {
+        MissionFailedBtn = (Button)findViewById(R.id.MissionFailedBtn);
+        OoofBtn = (Button)findViewById(R.id.OoofBtn);
+
+        MissionFailedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mp = MediaPlayer.create(GamesActivity.this, R.raw.missionfailed);
+                mp.start();
+            }
+        });
+
+        OoofBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MediaPlayer mp = MediaPlayer.create(GamesActivity.this, R.raw.ooof);
                 mp.start();
+
             }
         });
+
     }
 }
